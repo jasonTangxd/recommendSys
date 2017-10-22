206677c935cee852ba4526b91eb40783d80299b6
jason <jason.tangxd@gmial.com>
2017-10-22 16:03:08 +0800

    模拟用户请求，生产日志文件

---
+++ remd-web/src/test/java/com/xxo/web/HashMapTest.java
@@ -0,0 +1,21 @@
+package com.xxo.web;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapTest {
+
+	public static void main(String[] args) {
+		
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		
+		map.put("k2", 5);
+		map.put("k1", 5);
+		
+		for (String str : map.keySet()) {
+			System.out.println(str);
+		}
+		
+	}
+	
+}
---
+++ remd-web/src/test/java/com/xxo/web/MapInitDataTest.java
@@ -0,0 +1,37 @@
+package com.xxo.web;
+
+import com.google.common.collect.ImmutableMap;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MapInitDataTest {
+
+	private static final Map<String, String> myMap;
+	static
+	{
+		myMap = new HashMap<String, String>();
+		myMap.put("a", "b");
+		myMap.put("c", "d");
+	}
+
+
+
+	public static void main(String[] args) {
+
+		HashMap<String, String > h = new HashMap<String, String>(){{
+			put("a","b");
+		}};
+
+		Map<String, Integer> left = ImmutableMap.of("a", 1, "b", 2, "c", 3);
+		//或者
+		Map<String, String> test = ImmutableMap.<String, String>builder()
+				.put("k1", "v1")
+				.put("k2", "v2")
+    	.build();
+
+	}
+
+}
